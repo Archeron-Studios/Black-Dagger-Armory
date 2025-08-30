96cf07fb63fe16fabd116e9ddee277358483d738
SirSmity <gamemaster6484@gmail.com>
2025-08-30 14:37:20 -0400

    Hill Vest and Beef Selections

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -857,6 +857,80 @@ class cfgWeapons {
 		};
 	};
 
+	class BDA_Armor_Beefe: OPTRE_UNSC_M52D_Armor {
+		dlc="BDA";
+		author="Grif";
+		displayName = "M52D Body Armor (G. Beefe)";
+		picture="\BDA_Armor\BDA_Vests\data\icons\BDA_ODST_Armor";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor_Custom\BDA_Legs\data\BDA_Legs_Beefe_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Beefe_CO.paa"
+		};
+		class ItemInfo: ItemInfo {
+				hiddenSelections[]= {
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+	//			"A_KneesLeft",
+	//			"A_KneesRight",
+				"A_KneesMarLeft",
+				"A_KneesMarRight",
+	//			"A_ODST",
+	//			"A_TacPad",
+	//			"A_ShinArmorLeft",
+	//			"A_ShinArmorRight",
+	//			"AS_BaseLeft",
+	//			"AS_BaseRight",
+				"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+	//			"AS_ODSTSniperRight",
+	//			"AS_ODSTSniperLeft",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+	//			"AP_AR",
+				"AP_BR",
+				"AP_Canteen",
+				"AP_GL",
+	//			"AP_Knife",
+				"AP_MGThigh",
+				"AP_AR",
+				"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+				"AP_Sniper",
+	//			"AP_Thigh",
+	//			"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+	//			"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+			hiddenSelectionsTextures[]= {
+				"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+				"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+				"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+				"optre_unsc_units\army\data\ghillie_woodland_co",
+				"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Reynolds_CO.paa"
+			};
+		};
+	};
+
 	/*class BDA_Armor_Ryder: BDA_Armor_RF {
 		author = "Rib";
 		displayName = "M52D Body Armor (C. Ryder)";
@@ -898,18 +972,6 @@ class cfgWeapons {
 		displayName = "M52B Body Armor (L. Arturo)";
 		hiddenSelectionsTextures[]= {
 			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52B_Armor_Arturo_CO.paa"
-		};
-	};
-
-	class BDA_Armor_Beefe: BDA_Armor_RF {
-		author = "Grif";
-		displayName = "M52D Body Armor (G. Beefe)";
-		hiddenSelectionsTextures[]= {
-			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
-			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
-			"BDA_Armor_Custom\BDA_Legs\data\BDA_Legs_Beefe_CO.paa",
-			"optre_unsc_units\army\data\ghillie_woodland_co",
-			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Beefe_CO.paa"
 		};
 	};
 
@@ -1266,6 +1328,18 @@ class cfgWeapons {
 			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co",
 			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Bachman_CO.paa"
+		};
+	};
+
+	class BDA_Armor_Hill: BDA_Armor_RF {
+		author = "Grif";
+		displayName = "M52D Body Armor (B. Hill)";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Hill_CO.paa"
 		};
 	};
 	//end

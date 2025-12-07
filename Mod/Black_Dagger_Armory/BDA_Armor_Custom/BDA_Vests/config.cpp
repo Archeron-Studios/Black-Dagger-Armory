217f0fcefbc8e977b576db84a877001d5f0fa6d4
SirSmity <gamemaster6484@gmail.com>
2025-12-07 15:11:46 -0500

    Rye Helm and Vest

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -1359,6 +1359,18 @@ class cfgWeapons {
 		};
 	};
 
+	class BDA_Armor_Rye: BDA_Armor_MD {
+		author = "Grif";
+		displayName = "M52D Body Armor (A. Rye)";
+		hiddenSelectionsTextures[]= {
+			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_S5_Medic_CO",
+			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co",
+			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Rye_CO.paa"
+		};
+	};
+
 
 	//Pilots
 

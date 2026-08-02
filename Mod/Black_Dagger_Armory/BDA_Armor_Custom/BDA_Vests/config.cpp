55f6542a3b1f03f85a27cd960f898f62bfe798fb
Ribbington_v2 <plumridgerob@gmail.com>
2026-08-02 03:07:57 +0100

    Niki full, Griffin, Hannibal, Sparh, Sparky Helms

--- Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
+++ Mod/Black_Dagger_Armory/BDA_Armor_Custom/BDA_Vests/config.cpp
@@ -1104,11 +1104,11 @@ class cfgWeapons {
 		};
 	};
 	class BDA_Armor_Niki: BDA_Armor_RF {
-		author = "Rib";
+		author = "Rib & Apollo";
 		displayName = "M52D Body Armor (B. Niki)";
 		hiddenSelectionsTextures[] = {
 			"BDA_Armor\BDA_Vests\data\BDA_Vest_CO.paa",
-			"BDA_Armor\BDA_Vests\data\BDA_Armor_CO.paa",
+			"BDA_Armor\BDA_Vests\data\BDA_Armor_S6_CO.paa",
 			"BDA_Armor\BDA_Legs\data\BDA_Legs_CO.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co",
 			"BDA_Armor_Custom\BDA_Vests\data\BDA_M52D_Armor_Niki_CO.paa"
